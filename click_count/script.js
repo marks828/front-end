134e5654c99bb3aa7539e14ed5ba36384624b95c
Mark Sweet <marks828@gmail.com>
2017-11-13 09:20:59 -0500

    gradient tests

--- click_count/script.js
+++ click_count/script.js
@@ -1,20 +1,22 @@
 $(document).ready(()=>{
   let p = $('p')
-
+  let btn = $('button')
   let i = 0
 
+
   p.html(i)
-  $('.one').click( ()=>{
-    i++
-    p.html(i)
-  })
-  $('.ten').click(()=>{
-    i+=10
-    p.html(i)
-  })
-  $('.hundred').click(()=>{
-    i+=100
-    p.html(i)
-  })
+  // $('.one').click( ()=>{
+  //   i++
+  //   p.html(i)
+  // })
+  // $('.ten').click(()=>{
+  //   i+=10
+  //   p.html(i)
+  // })
+  // $('.hundred').click(()=>{
+  //   i+=100
+  //   p.html(i)
+  // })
+
 
 })
