1b78b7c9df1eca985b7f159d3ea4d9dc2d8b6ebe
Mark Sweet <marks828@gmail.com>
2017-11-17 09:57:08 -0500

    random background color with added new color input

--- background-change/script.js
+++ background-change/script.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
 
+  // variables
   let body = $('body')
-  let colors = ["red", "blue", "green", "lightblue"]
+  let change = $('.change')
+  let colors = ['#1a1a1a', "blue", "green", "lightblue"]
+  let shuffleArray = shuffle(colors)
+  let addNewColor = $('.addNewColor')
+  let input = $('input')
 
+  // shuffle function
+  function shuffle(array){
+    for(let i=array.length - 1; i>0; i--){
+      var j = Math.floor(Math.random()*(i+1))
+      var temp = array[i]
+      array[i]=array[j]
+      array[j]=temp
+    }
+    return array
+  }
 
-  
+  body.css("background-color", shuffleArray[0])
 
-  body.css("background-color", )
+  // button click to change background
+  change.click(function(){
+    let newBackground = shuffle(colors)
+    body.css("background-color", newBackground[0])
+  })
 
+  // ADD NEW COLOR
+  // take input value and push into colors array
+  addNewColor.click(function(){
+    let newColor = input.val()
+    colors.push(newColor)
+    console.log(colors);
+    input.val('')
+  })
 })
